949e21cca0953fb4694cbb0dfb77480c1883ccb0
RobsonAlmeida <robsonalmeida073@gmail.com>
2022-01-03 23:56:23 -0300

    Controlando o navegador

---
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -0,0 +1,24 @@
+package br.com.alura.leilao;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWorldSelenium {
+
+	@Test
+	public void helloTest() {
+		WebDriver browser;
+		//Chrome
+//                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//                browser = new ChromeDriver();
+		//Firefox
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+		browser = new FirefoxDriver();
+                
+                
+                browser.navigate().to("http://localhost:8080/leiloes");
+                browser.quit();
+	}
+
+}
