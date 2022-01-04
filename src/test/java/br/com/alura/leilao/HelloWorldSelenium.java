bebc9043ca791c44e096965c35a8640ad4504dc2
RobsonAlmeida <robsonalmeida073@gmail.com>
2022-01-04 15:49:01 -0300

    Testando o login

--- src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
+++ src/test/java/br/com/alura/leilao/HelloWorldSelenium.java
@@ -6,19 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWorldSelenium {
 
-	@Test
-	public void helloTest() {
-		WebDriver browser;
-		//Chrome
-//                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-//                browser = new ChromeDriver();
-		//Firefox
-                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-		browser = new FirefoxDriver();
-                
-                
-                browser.navigate().to("http://localhost:8080/leiloes");
-                browser.quit();
-	}
+    @Test
+    public void helloTest() {
+        WebDriver browser;
+        //Chrome
+//        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//        browser = new ChromeDriver();
+        //Firefox
+        System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+        browser = new FirefoxDriver();
+
+        browser.navigate().to("http://localhost:8080/leiloes");
+        browser.quit();
+    }
 
 }
